632e4ed2b23e5a223ecde5b1b673d7f46c544313
Graham Fairweather <xotic750@gmail.com>
2019-07-15 14:54:37 +0200

    :bookmark: v4.0.2
    Auto build

--- dist/array-slice-x.js
+++ dist/array-slice-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-15T10:57:53.528Z",
+  "date": "2019-07-15T12:54:32.701Z",
   "describe": "",
   "description": "Cross-browser array slicer.",
   "file": "array-slice-x.js",
   "hash": "4f7e79c03c7e95f24a0f",
   "license": "MIT",
-  "version": "4.0.1"
+  "version": "4.0.2"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
