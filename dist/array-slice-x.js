b231c1076bb6352a3c4cc56deb24e8359eecdf17
Graham Fairweather <xotic750@gmail.com>
2019-07-31 19:27:38 +0200

    Package rework

--- dist/array-slice-x.js
+++ dist/array-slice-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-31T15:58:37.767Z",
+  "date": "2019-07-31T17:27:32.194Z",
   "describe": "",
   "description": "Cross-browser array slicer.",
   "file": "array-slice-x.js",
